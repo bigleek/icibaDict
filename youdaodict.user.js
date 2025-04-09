5bbef4034658c5610c6f50f499c73ebab7ef33d0
bigleek <39549156+bigleek@users.noreply.github.com>
2025-04-09 16:54:00 +0800

    Update youdaodict.user.js

--- youdaodict.user.js
+++ youdaodict.user.js
@@ -1,22 +1,23 @@
 // ==UserScript==
-// @id             youdaodict-greasemonkey-reverland-2015-09-26
-// @name           youdaodict
-// @name:zh-CN     有道取词
+// @id             iciba-greasemonkey-reverland-2025-4-9
+// @name           iciba
+// @name:zh-CN     金山词霸取词
 // @version        2.0
-// @namespace      https://github.com/HalfdogStudio/youdaodict
-// @author         Liu Yuyang(sa@linuxer.me)
+// @namespace      https://github.com/bigleek/icibaDict
+// @author         bigleek(bigleekcode@gmail.com)
 // @description    Translate any text selected into a tooltip
 // @description:zh-cn    一个可以在浏览器中自由使用的屏幕取词脚本
 // @include        *
-// @require  https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @require  https://github.com/bigleek/icibaDict/raw/refs/heads/master/youdaodict.user.js
 // @grant          GM_xmlhttpRequest
 // @grant          GM.xmlHttpRequest
+// @license MIT
 // ==/UserScript==
-
+ 
 window.document.body.addEventListener("mouseup", translate, false);
 window.document.body.addEventListener("keyup", toggleYoudao, false);
 var toggle = true;
-
+ 
 function toggleYoudao(e) {
   if (e.which == 81 && e.altKey && e.ctrlKey) {
     if (toggle) {
@@ -28,7 +29,7 @@ function toggleYoudao(e) {
     }
   }
 }
-
+ 
 function translate(e) {
   // remove previous .youdaoPopup if exists
   var previous = document.querySelector(".youdaoPopup");
@@ -37,7 +38,7 @@ function translate(e) {
   }
   //console.log("translate start");
   var selectObj = document.getSelection();
-
+ 
   // if #text node
   if (selectObj.anchorNode && selectObj.anchorNode.nodeType == 3) {
     //GM_log(selectObj.anchorNode.nodeType.toString());
@@ -55,9 +56,9 @@ function translate(e) {
     var mx = e.clientX;
     var my = e.clientY;
     translate(word, ts);
-
+ 
   }
-
+ 
   function popup(mx, my, result) {
     //console.log(mx)
     //console.log(my)
@@ -67,9 +68,8 @@ function translate(e) {
     // parse
     var dictJSON = JSON.parse(result);
     console.log(dictJSON);
-    var query = dictJSON['query'];
-    var errorCode = dictJSON['errorCode'];
-    if (dictJSON['basic']) {
+    var paraphrase = dictJSON.message[0].paraphrase;
+    if (!(paraphrase ==='')) {
       word();
     } else {
       sentence();
@@ -98,19 +98,19 @@ function translate(e) {
     }
     youdaoWindow.style.padding = "5px";
     youdaoWindow.style.zIndex = '999999';
-
+ 
     function word() {
-
+ 
       function play(word) {
         //console.log("[DEBUG] PLAYOUND")
-
+ 
         function playSound(buffer) {
           var source = context.createBufferSource();
           source.buffer = buffer;
           source.connect(context.destination);
           source.start(0);
         }
-
+ 
         var context = new AudioContext();
         var soundUrl = `https://dict.youdao.com/dictvoice?type=2&audio=${word}`;
         var p = new Promise(function(resolve, reject) {
@@ -133,16 +133,16 @@ function translate(e) {
           console.log(e);
         });
       }
-
-      var basic = dictJSON['basic'];
+ 
+      var basic = dictJSON.message[0];
       var header = document.createElement('p');
       // header
       var span = document.createElement('span');
-      span.innerHTML = query;
+      span.innerHTML = "" + dictJSON.message[0].key + "";
       header.appendChild(span);
       // phonetic if there is
-      var phonetic = basic['phonetic'];
-      if (phonetic) {
+     // var phonetic = basic['phonetic'];
+      if (false) {
         var phoneticNode = document.createElement('span');
         phoneticNode.innerHTML = '[' + phonetic + ']';
         phoneticNode.style.cursor = "pointer";
@@ -161,7 +161,7 @@ function translate(e) {
       header.style.padding = "0";
       span.style.fontweight = "900";
       span.style.color = "black";
-
+ 
       youdaoWindow.appendChild(header);
       var hr = document.createElement('hr');
       hr.style.margin = "0";
@@ -173,42 +173,46 @@ function translate(e) {
       // ul style
       ul.style.margin = "0";
       ul.style.padding = "0";
-      basic['explains'].map(function(trans) {
+      basic['means'].map(function(trans) {
         var li = document.createElement('li');
         li.style.listStyle = "none";
         li.style.margin = "0";
         li.style.padding = "0";
         li.style.background = "none";
         li.style.color = "inherit";
-        li.appendChild(document.createTextNode(trans));
+        li.appendChild(document.createTextNode(trans.part+trans.means));
         ul.appendChild(li);
       });
       youdaoWindow.appendChild(ul);
-
+ 
     }
-
+ 
     function sentence() {
       var ul = document.createElement('ul');
       // ul style
       ul.style.margin = "0";
       ul.style.padding = "0";
-      dictJSON['translation'].map(function(trans) {
+      var sentence = dictJSON.message[0];
+ 
+      sentence['means'].map(function(trans) {
         var li = document.createElement('li');
         li.style.listStyle = "none";
         li.style.margin = "0";
         li.style.padding = "0";
         li.style.background = "none";
         li.style.color = "inherit";
-        li.appendChild(document.createTextNode(trans));
+        li.appendChild(document.createTextNode(trans.part+trans.means));
         ul.appendChild(li);
       });
       youdaoWindow.appendChild(ul);
     }
   }
-
-
+ 
+ 
   function translate(word, ts) {
-    var reqUrl = `http://fanyi.youdao.com/openapi.do?type=data&doctype=json&version=1.1&relatedUrl=http%3A%2F%2Ffanyi.youdao.com%2F%23&keyfrom=fanyiweb&key=null&translate=on&q=${word}&ts=${ts}`;
+ 
+ 
+    var reqUrl = `https://dict-mobile.iciba.com/interface/index.php?c=word&m=getsuggest&nums=10&is_need_mean=1&word=${word}&ts=${ts}`;
     //console.log("request url: ", reqUrl);
     var ret = GM.xmlHttpRequest({
       method: "GET",
@@ -228,4 +232,3 @@ function translate(e) {
     });
   }
 }
-
